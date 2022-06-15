559c50259f8408148536ba2fd2a3d1a91cc93838
llthanh714 <llthanh714@live.com>
2022-06-15 21:11:42 +0700

    add cors when fetch data

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,13 +1,23 @@
 var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/vaccination/23041991';
+
 function getItems() {
-    fetch(uri, { mode: 'no-cors'})
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (text) {
-            alert('text');
-        })
-        .catch(function (error) {
-            alert(error);
+    fetch(uri, {mode: "cors"})
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Error:: ' + response.status);
+                    return;
+                }
+                response.json().then(data => {
+                    console.log(data);
+                })
+            }
+        )
+        .catch(err => {
+            console.log('Error: ', err)
         });
 };
+
+function showDetail(item) {
+    alert(item);
+}
