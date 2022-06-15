d58b76f89623353712be1e9cf1f2b47fe60cf6da
llthanh714 <llthanh714@live.com>
2022-06-15 20:29:02 +0700

    Fix fetch data

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,8 +1,13 @@
 var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/vaccination/23041991';
 function getItems() {
-    alert(uri);
-    fetch(uri)
-        .then(response => response.json())
-        .then(data => alert(data))
-        .catch(error => console.error('Unable to get items.', error));
+    fetch(uri, { mode: 'no-cors'})
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (text) {
+            alert('text');
+        })
+        .catch(function (error) {
+            alert(error);
+        });
 };
