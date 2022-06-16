cfe276abe35a38d3a12ad6c6e3f54b63403695fe
llthanh714@live.com <llthanh714@live.com>
2022-06-16 08:17:55 +0700

    Edit html data

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,15 +1,16 @@
-var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/vaccination/23041991';
+var uri = 'http://localhost:9839/api/3rd/vaccination/23041991';
 
 function getItems() {
-    fetch(uri, {mode: "cors"})
+    document.getElementById('table-detail').innerHTML = "";
+    fetch(uri, { mode: "cors" })
         .then(
             function (response) {
                 if (response.status !== 200) {
-                    console.log('Error:: ' + response.status);
+                    console.log('Error: ' + response.status);
                     return;
                 }
                 response.json().then(data => {
-                    console.log(data);
+                    data.forEach(addDetail);
                 })
             }
         )
@@ -18,6 +19,9 @@ function getItems() {
         });
 };
 
-function showDetail(item) {
-    alert(item);
-}
+function addDetail(item, index, array) {
+    var obj = Object.values(item);
+    var html = '<tr>' + '<td>' + obj[0] + '</td><td>' + obj[1] + '</td><td>' + obj[2] + '</td></tr>';
+    const table = document.getElementById("table-detail");
+    table.innerHTML += html;
+};
