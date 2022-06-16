019e5cae620e0f28a276ba937955b34c9a746278
llthanh714 <llthanh714@live.com>
2022-06-16 15:59:24 +0700

    fix fetch data

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,9 +1,9 @@
 //var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/vaccination/';
-var uri = ' http://localhost:9839/api/3rd/vaccination/';
+var uri = ' http://localhost:9839/api/3rd/';
 function getItems() {
     document.getElementById('table-detail').innerHTML = "";
     var date = document.getElementById('birthday').value.replace('/','').replace('/','');
-    fetch(uri + date, { mode: "cors" })
+    fetch(uri + 'vaccination/' + date, { mode: "cors" })
         .then(
             function (response) {
                 if (response.status !== 200) {
@@ -20,6 +20,24 @@ function getItems() {
         });
 };
 
+function getDetail(name) {
+    fetch(uri + 'detail/' + name, { mode: "cors" })
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Error: ' + response.status);
+                    return;
+                }
+                response.json().then(data => {
+                    data.forEach(showDetail);
+                })
+            }
+        )
+        .catch(err => {
+            console.log('Error: ', err)
+        });
+};
+
 function addDetail(item, index, array) {
     var obj = Object.values(item);
     var html = '<tr>' + '<td>' + obj[0] + '</td><td>' + obj[1] + '</td><td>' + obj[2] + '</td></tr>';
@@ -27,7 +45,7 @@ function addDetail(item, index, array) {
     table.innerHTML += html;
 };
 
-function showDetail(item){
+function showDetail(item, index, array){
     var obj = Object.values(item);
     const title = document.getElementById("modalTitle");
     title.innerHTML = obj[0];
