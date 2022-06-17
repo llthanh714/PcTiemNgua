dd5a1c534b81d502f737f4f6e6860a8a56bee4d9
llthanh714@live.com <llthanh714@live.com>
2022-06-17 09:31:00 +0700

    Fix bug

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,5 +1,5 @@
-//var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/vaccination/';
-var uri = 'https://localhost:5001/api/3rd/';
+var uri = 'http://113.161.210.94:9001/pcportal/api/3rd/';
+//var uri = 'https://localhost:5001/api/3rd/';
 
 function getItems() {
     document.getElementById('table-detail').innerHTML = "";
